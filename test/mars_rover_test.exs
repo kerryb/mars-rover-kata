c7fc5fe4201c91177a2014c1aff9e63837bfb988
Kerry Buckley <kerryjbuckley@gmail.com>
2021-09-02 15:04:59 +0100

    Add failing acceptance tests

--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -1,4 +1,13 @@
 defmodule MarsRoverTest do
   use ExUnit.Case
-  doctest MarsRover
+
+  describe "MarsRover.execute/1" do
+    test "returns the correct final position for a simple path" do
+      assert MarsRover.execute("MMRMMLM") == "2:3:N"
+    end
+
+    test "returns the correct final position for a path that wraps round the edge of the grid" do
+      assert MarsRover.execute("MMMMMMMMMM") == "0:0:N"
+    end
+  end
 end
