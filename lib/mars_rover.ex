0ae90491b0df8054fb0f49f186a13bfbac459c94
Kerry Buckley <kerryjbuckley@gmail.com>
2021-09-02 19:24:25 +0100

    Wire up Rover to main MarsRover module

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,2 +1,16 @@
 defmodule MarsRover do
+  alias MarsRover.Rover
+
+  def execute(command_string) do
+    command_string
+    |> split()
+    |> execute_in_turn(Rover.new())
+    |> to_string()
+  end
+
+  defp split(command_string), do: String.codepoints(command_string)
+
+  defp execute_in_turn(commands, rover) do
+    Enum.reduce(commands, rover, fn command, rover -> Rover.command(rover, command) end)
+  end
 end
